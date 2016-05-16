05bfc92e9b21f8d2878d1c46e8dcb2e2dccc9771
cree <andersan@umich.edu>
2016-05-16 09:47:24 -0400

    added penn data to views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
+from flask import render_template, request
 from app import app
+import sqlalchemy, sqlite3
+from sqlalchemy import create_engine, MetaData, Table
+
+engine = create_engine('sqlite:///:main')
+meta = MetaData(bind=engine)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "cigs"
+		Penn = Table('penn_complete', meta, autoload=True)
+		Legend = Table('legend', meta, autoload=True)
+		return render_template('index.html', **locals())
