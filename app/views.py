140b46019d2d0bf46d28fe1a3ffd404c7d3b65e4
cree <andersan@umich.edu>
2016-05-20 04:44:01 -0400

    minor updates to views and templates, testing out backend communication w front-end

--- app/views.py
+++ app/views.py
@@ -1,15 +1,22 @@
 from flask import render_template, request
 from app import app
 import sqlalchemy, sqlite3
-from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy import create_engine, MetaData, Table, select, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import table, literal_column
+from db_create import * 
+from functions import * 
 
-engine = create_engine('sqlite:///:main')
+engine = create_engine('sqlite:///:circa')
 meta = MetaData(bind=engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 @app.route('/')
 @app.route('/index')
 def index():
-		Penn = Table('penn_complete', meta, autoload=True)
 		Legend = Table('legend', meta, autoload=True)
-		Map = render_template('south_america.svg')
+		World = Table('world_map_data', meta, autoload=True)
+		SA_Penn = Table('pwt8_sa', meta, autoload=True)
+		cc = session.query(Penn_South_America.countrycode)
 		return render_template('index.html', **locals())
