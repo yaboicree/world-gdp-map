d53ef2bb7ea59aa27efe49dafd0494b233ce3442
cree <andersan@umich.edu>
2016-05-20 04:41:08 -0400

    updated models

--- db_create.py
+++ db_create.py
@@ -14,33 +14,21 @@ class Legend(Base):
 
 	var_name = Column(String, primary_key=True)
 	var_desc = Column(String)
-
-class Penn(Base):
-	__tablename__ = 'penn'
-
-	countrycode = Column(String, primary_key=True)
-	country = Column(String)
-	year = Column(Integer)
-	rgdpna = Column(Float) # Real GDP PPP-2005 in USD 
-	pop = Column(Integer) # population
-	ck = Column(Float) # capital stock 
-	hc = Column(Float) # educational attainment
-	csh_x = Column(Float) # exports
-	csh_m = Column(Float) # imports
+	index = Column(Integer)
 
 # countries chosen for svg map ("PAN","NIC","VEN","URY","SUR","PER","PRY","GUY","GUF","FLK","ECU","COL","CHL","BRA","BOL","ARG")
 class SA_Countries(Base):
- 	__tablename__ = "SA_Countries"
+ 	__tablename__ = "sa_countries"
 
  	countrycode = Column(String, primary_key=True)
 
 # import from pwt sheet 'Data'
-class Penn_Complete(Base):
-	__tablename__ = 'PWT_8.1'
+class Penn_South_America(Base):
+	__tablename__ = 'pwt8_sa'
 
-	countrycode = Column(String, primary_key=True)
-	country = Column(String)
-	currency_unit = Column(Float)
+	countrycode = Column(String)
+	country = Column(String(64))
+	currency_unit = Column(String(64))
 	year = Column(Integer)
 	rgdpe = Column(Float)
 	rgdpo = Column(Float)
@@ -67,10 +55,10 @@ class Penn_Complete(Base):
 	pl_con = Column(Float)
 	pl_da = Column(Float)
 	pl_gdpo = Column(Float)
-	i_cig = Column(String)
-	i_xm = Column(String)
-	i_xr = Column(String)
-	i_outlier = Column(String)
+	i_cig = Column(String(64))
+	i_xm = Column(String(64))
+	i_xr = Column(String(64))
+	i_outlier = Column(String(64))
 	cor_exp = Column(Float)
 	statcap = Column(Float)
 	csh_c = Column(Float)
@@ -85,6 +73,61 @@ class Penn_Complete(Base):
 	pl_x = Column(Float)
 	pl_m = Column(Float)
 	pl_k = Column(Float)
+	index = Column(Integer, primary_key=True)
+
+# import from pwt sheet 'Data'  
+##--- TOO MUCH DATA (4mb), DO NOT IMPORT ---##
+class Penn_Complete(Base):
+	__tablename__ = 'pwt8'
+
+	countrycode = Column(String)
+	country = Column(String(64))
+	currency_unit = Column(String(64))
+	year = Column(Integer)
+	rgdpe = Column(Float)
+	rgdpo = Column(Float)
+	pop = Column(Float)
+	emp = Column(Float)
+	avh = Column(Float)
+	hc = Column(Float)
+	ccon = Column(Float)
+	cda = Column(Float)
+	cgdpe = Column(Float)
+	cgdpo = Column(Float)
+	ck = Column(Float)
+	ctfp = Column(Float)
+	cwtfp = Column(Float)
+	rgdpna = Column(Float)
+	rconna = Column(Float)
+	rdana = Column(Float)
+	rkna = Column(Float)
+	rtfpna = Column(Float)
+	rwtfpna = Column(Float)
+	labsh = Column(Float)
+	delta = Column(Float)
+	xr = Column(Float)
+	pl_con = Column(Float)
+	pl_da = Column(Float)
+	pl_gdpo = Column(Float)
+	i_cig = Column(String(64))
+	i_xm = Column(String(64))
+	i_xr = Column(String(64))
+	i_outlier = Column(String(64))
+	cor_exp = Column(Float)
+	statcap = Column(Float)
+	csh_c = Column(Float)
+	csh_i = Column(Float)
+	csh_g = Column(Float)
+	csh_x = Column(Float)
+	csh_m = Column(Float)
+	csh_r = Column(Float)
+	pl_c = Column(Float)
+	pl_i = Column(Float)
+	pl_g = Column(Float)
+	pl_x = Column(Float)
+	pl_m = Column(Float)
+	pl_k = Column(Float)
+	index = Column(Integer, primary_key=True)
 
 # geographic data from mapping dataset 
 # use iso3 field to match to countrycode fields in other tables
@@ -92,12 +135,12 @@ class World(Base):
 	__tablename__ = 'world_map_data'
 
 	iso3 = Column(String, primary_key=True)
-	name = Column(String)
 	area = Column(Integer)
 	pop2005 = Column(Integer)
 	region = Column(Integer)
 	lon = Column(Float)
 	lat = Column(Float)
+	index = Column(Integer)
 
 # commit all schemas to sqlite3 database 'circa' 
 Base.metadata.create_all(engine)
--- import_pandas.py
+++ import_pandas.py
@@ -1,13 +1,26 @@
 import pandas, sqlite3, sqlalchemy
+from numpy import ndarray
 from sqlalchemy import *
 
 # pandas to read csv files
-legend = pandas.read_csv('pwt8.1_LEGEND.csv')
-data = pandas.read_csv('pwt8.1.csv')
+legend = pandas.read_csv('data/pwt8.1_LEGEND.csv')
+data = pandas.read_csv('data/pwt8.1.csv', encoding='utf-8')
+
+# import data to identify locations of countries and their country codes
+world = pandas.read_csv('data/borders.csv')
+# drop unnecessary columns (other country naming conventions, subregion field)
+world.drop(world.columns[[0,1,3,4,8]], axis=1, inplace=True)
 
 # sqlalchemy to import to database ??
-engine = create_engine('sqlite:///:main')
+engine = create_engine('sqlite:///:circa')
 
 # use pandas to write to database, sqlalchemy creates connection
 legend.to_sql('legend', engine, if_exists='append')
-data.to_sql('penn_complete', engine, if_exists='append')
+# data.to_sql('PWT_8.1', engine, if_exists='append')
+world.to_sql('world_map_data', engine, if_exists='append')
+
+SA_Countries = ("PAN","NIC","VEN","URY","SUR","PER","PRY","GUY","GUF","FLK","ECU","COL","CHL","BRA","BOL","ARG")
+
+sa_data = data.loc[data['countrycode'].isin(SA_Countries)]
+# sa_data[['countrycode','country','year','rgdpna','pop','ck','hc','csh_x','csh_m','index']
+sa_data.to_sql('pwt8_sa', engine, if_exists='append')
